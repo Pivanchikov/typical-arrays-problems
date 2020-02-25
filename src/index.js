67f8e2c53af61731799c4c805a1f763bd9078039
Pivanchikov <56319613+Pivanchikov@users.noreply.github.com>
2020-02-25 13:58:41 +0300

    init: start  algorithmic task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (arr){
+    let number = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (number > arr[i]) {
+            number = arr[i];
+        }
+    }
+    return number;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (arr){
+    let number = 0;
+    for (let i = 0; i < arr.length; i++) {
+        if (number < arr[i]) {
+            number = arr[i];
+        }
+    }
+    return number;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (arr){
+    let number = 0;
+    let counter = 0;
+    for (let i = 0; i < arr.length; i++) {
+        number += arr[i];
+        counter++ 
+    }
+    number = number / counter;
+    return number;
 }
